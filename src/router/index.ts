2797ca6bdb136001c56d4ba231ab12d93fb79b13
Yurii Musolov <yurii.musolov@gmail.com>
2023-08-26 19:13:43 +0300

    feat(views): add info page

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/info',
+      name: 'info',
+      // route level code-splitting
+      // this generates a separate chunk (Info.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/InfoView.vue')
     }
   ]
 })
---
+++ src/views/InfoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="info">
+    <h1>This is an info page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .info {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
